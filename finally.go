78c0bb3c3aca15097a3345add1d30382b6109f28
yifeiw <chrisww@live.com>
2019-12-22 22:05:03 -0500

    Add documentation

--- finally.go
+++ finally.go
@@ -13,12 +13,20 @@ import (
 type FinallyHandler func()
 type FinallyHandlerSig func(sig os.Signal)
 
+// `Wrap` wraps a function of type `func()` and returns another
+// function to call the input function . The input function will be
+// called when the program receives shutdown signals or the returned
+// function is called. The input function is guranteed to be invoked
+// only once.
 func Wrap(handler FinallyHandler) func() {
 	c := newHandlerContext()
 	c.handler = handler
 	return c.executeNoSig
 }
 
+// `WrapSig` does exactly the same thing as `Wrap`, except that is
+// accepts an argument representing the signal program received. If
+// the wrapping function is called, the signal will be nil.
 func WrapSig(handler FinallyHandlerSig) func() {
 	c := newHandlerContext()
 	c.acceptArgument = true
@@ -26,6 +34,9 @@ func WrapSig(handler FinallyHandlerSig) func() {
 	return c.executeNoSig
 }
 
+// `RegisterShutdownHook` register signal handlers as the arguments
+// specified. If no arguments are specified, os.Interrupt and
+// syscall.SIGTERM handlers are registered.
 func RegisterShutdownHook(signals ...os.Signal) {
 	ch := make(chan os.Signal)
 
@@ -48,6 +59,9 @@ func RegisterShutdownHook(signals ...os.Signal) {
 	}()
 }
 
+// `SetRecordStackTrace` set the config about whether the stacktrace
+// should be save so that when a panic happenes in a finally handler,
+// the stacktrace could be shown.
 func SetRecordStackTrace(v bool) {
 	recordStackTrace = v
 }
